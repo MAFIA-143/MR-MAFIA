c3e02e4db74f284bae787f4dc144be5b8a559014
𝗠𝗥 𝗠𝗔𝗙𝗜𝗔 <msrachadleroi01@hotmail.com>
2025-01-22 12:21:18 +0100

    Update run.py

--- run.py
+++ run.py
@@ -9,13 +9,11 @@ if fuck == '64bit':
     time.sleep(2)
     from f64 import MX1
     MX1()
-    return None
 if fuck == '32bit':
     os.system('git pull')
     os.system('clear')
     print('\x1b[1;94m[√] \x1b[1;92mYour Device is 32 bit')
     time.sleep(2)
-    exit()
-    return None
+    exit() 
 os.system('clear')
 print('\x1b[1;97m Soon Your Device Supported Tools ')
