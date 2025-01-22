1bcb140774d9a8eb37940aded119bc9944d6ed1e
𝗠𝗥 𝗠𝗔𝗙𝗜𝗔 <msrachadleroi01@hotmail.com>
2025-01-22 12:20:17 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
 import os
 import platform
+import time
+fuck = platform.architecture()[0]
+if fuck == '64bit':
+    os.system('git pull')
+    os.system('clear')
+    print('\x1b[1;94m[√] \x1b[1;92mYour Device is 64 bit')
+    time.sleep(2)
+    from f64 import MX1
+    MX1()
+    return None
+if fuck == '32bit':
+    os.system('git pull')
+    os.system('clear')
+    print('\x1b[1;94m[√] \x1b[1;92mYour Device is 32 bit')
+    time.sleep(2)
+    exit()
+    return None
 os.system('clear')
-os.system("git pull")
-b = platform.architecture()[0]
-if b == '64bit':
-    import MX1
-elif b == '32bit':
-    import MX2
-
+print('\x1b[1;97m Soon Your Device Supported Tools ')
