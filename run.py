b720ae8cdaf49c611f6c9471b010c60fd1d880b5
𝗠𝗥 𝗠𝗔𝗙𝗜𝗔 <msrachadleroi01@hotmail.com>
2025-02-27 09:12:01 +0600

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ if fuck == '64bit':
     os.system('clear')
     print('\x1b[1;94m[√] \x1b[1;92mYour Device is 64 bit')
     time.sleep(2)
-    from MX3 import MX3
+    from run import run
     MX3()
 if fuck == '32bit':
     os.system('git pull')
